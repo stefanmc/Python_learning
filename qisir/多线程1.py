df0676ed05961b0cdc1120e6234187555569c29c
stefan.xsz@msn.cn <stefan.xsz@msn.cn>
2022-11-14 23:03:34 +0800

    update

---
+++ qisir/多线程1.py
@@ -0,0 +1,34 @@
+# coding:utf-8
+
+import threading
+from time import ctime, sleep
+
+
+def movie():
+    print('now is moving' + str(ctime()))
+    sleep(2)
+    print('ending ....' + str(ctime()))
+
+
+def music():
+    print('now is musicing' + str(ctime()))
+    sleep(3)
+    print('ending ...' + str(ctime()))
+
+
+if __name__ == '__main__':
+    threads = []
+    threads1 = threading.Thread(target=movie)
+    threads.append(threads1)
+    threads2 = threading.Thread(target=music)
+    threads.append(threads2)
+
+    for i in threads:
+        i.start()
+
+    for t in threads:
+        t.join()
+
+    print('结束' + str(ctime()))
+    # movie()
+    # music()
