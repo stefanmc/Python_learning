2468cf01fe48b72a4ca20c290f0f257fbab4f75b
stefan.xsz@msn.cn <stefan.xsz@msn.cn>
2024-04-17 21:56:25 +0800

    update

--- tmp.py
+++ tmp.py
@@ -2,7 +2,17 @@ list_a = '@029800@311100@204200@050099@399352@399353@399354@395590@398670@396586
 
 list_b = '3333'
 line = list_b.split('@')
-print(line)
-print(line[1:])
+# print(line)
+# print(line[1:])
 # lines = tuple(line[1:])
 # print(line)
+
+
+x = iter(range(1, 10))
+# result = list(zip(x,x,x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
+print(next(x))
